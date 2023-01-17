1ec38c993606679a5762229284829fb7580c161f
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2023-01-17 21:12:42 +0000

    update

--- script.js
+++ script.js
@@ -6,22 +6,24 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function renderScene() {
-	var x = document.getElementById("startButton");
-	var t = document.getElementById("container");
-	if (x.innerHTML === "Off") {
-	  x.innerHTML = "On";
-	t.style.gridTemplate = `
+	var x = document.getElementById('startButton');
+	var t = document.getElementById('container');
+	var i = document.getElementById('bitch');
+	if (x.innerHTML === 'Off') {
+		x.innerHTML = 'On';
+		i.style.backgroundColor = 'green';
+		t.style.gridTemplate = `
 	'm m m' 1fr
 	's s s' 1fr
 	'h h h' 1fr
 	/ [col-start] 1fr [col-2] 1fr [col-3] 1fr [col-end]`;
-}
-	  else {
-	  x.innerHTML = "Off";
-	  t.style.gridTemplate = `
+	} else {
+		x.innerHTML = 'Off';
+		i.style.backgroundColor = 'blue';
+		t.style.gridTemplate = `
 	's m h' 1fr
 	's m h' 1fr
 	's m h' 1fr
 	/ [col-start] 1fr [col-2] 1fr [col-3] 1fr [col-end]`;
 	}
-  }
+}
