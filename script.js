978ede1d03c48214c9c1a352224738b742b42990
hramzan01 <112130674+hramzan01@users.noreply.github.com>
2023-01-18 15:25:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,25 +8,26 @@ document.addEventListener('DOMContentLoaded', function () {
 function renderScene() {
 	var x = document.getElementById('startButton');
 	var t = document.getElementById('container');
-	var i = document.getElementById('itch');
+	var f = document.getElementById('DAP')
 	var y = document.getElementsByClassName('item')
 	for(var p=0; p<y.length; p++)
 	if (x.innerHTML === 'Off') {
 		x.innerHTML = 'On';
-		i.style.backgroundColor = 'green';
 		t.style.gridTemplate = `
 		'm m m' 1fr
 		's s s' 1fr
 		'h h h' 1fr
 		/ [col-start] 1fr [col-2] 1fr [col-3] 1fr [col-end]`;
 		y[p].style.backgroundColor = 'red';
+		f.innerHTML = 'Development Area Programme'
 	} else {
 		x.innerHTML = 'Off';
-		i.style.backgroundColor = 'blue';
 		t.style.gridTemplate = `
-	's m h' 1fr
-	's m h' 1fr
-	's m h' 1fr
-	/ [col-start] 1fr [col-2] 1fr [col-3] 1fr [col-end]`;
+		's m h' 1fr
+		's m h' 1fr
+		's m h' 1fr
+		/ [col-start] 1fr [col-2] 1fr [col-3] 1fr [col-end]`;
+		y[p].style.backgroundColor = 'green';
+		f.innerHTML = '<img src = "img/1_2X4kX8wkfbtBFu9Zack_IA.gif">'
 	}
 }
